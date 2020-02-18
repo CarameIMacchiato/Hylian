fed0e0e1c82eb54272a3df72c349583af2aa8767
Carmelito <carmelitogutierrez@gmail.com>
2020-02-17 20:28:39 -0800

    add images fix spacing and add footer

--- gatsby-site/src/components/layout.js
+++ gatsby-site/src/components/layout.js
@@ -37,11 +37,13 @@ const Layout = ({ children }) => {
         
         <main>{children}</main>
         <Team></Team>
-        <footer>
-          © {new Date().getFullYear()}, Built with
+        
+        <footer class="footer">
+          iSchool Capstone 2020, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
+
       </div>
     </>
   )
