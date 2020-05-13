1265788dc561798a2a83b843ed9d936b29656ee6
Alex Wong <alwong34@uw.edu>
2020-05-13 15:09:01 -0700

    added infographic

--- src/components/infographic.js
+++ src/components/infographic.js
@@ -1,13 +1,16 @@
 import React from "react"
 import "./layout.css"
+import graphic from "../images/CapstoneInfographic-02.png"
 
 const Infographic = () => {
     return (
         <div>
-            <div class="skewed-right" style={{background: '#657984', paddingTop: '10px'}}></div>
-            <div style={{background: '#657984', height: '100vh', width: '100%'}}>
+            <div class="skewed-right" style={{background: '#657984', paddingTop: '10px', height: '100px'}}></div>
+            <div style={{position: 'relative', background: '#657984', height: '57rem', width: '100%', zIndex: '0', paddingTop: '5rem'}}>
+                <a href={graphic} target="_blank">
+                    <img src={graphic} alt={'accessible gaming infographic'} style={{display: 'block', margin: 'auto', width: '70%'}}></img>
+                </a> 
             </div>
-            {/* <div class="skewed-left" style={{background: '#657984'}}></div> */}
         </div>
     )
 }
