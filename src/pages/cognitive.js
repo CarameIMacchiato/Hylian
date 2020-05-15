9788dfb8bb914d0c25223f18bf97ed6f36aac7d9
Carmelito <carmelitogutierrez@gmail.com>
2020-05-15 10:29:33 -0700

    add text

--- src/pages/cognitive.js
+++ src/pages/cognitive.js
@@ -52,38 +52,117 @@ const cognitive = () => {
                             game elements as hints to the player
                         </p>
                     </div>
-                    <div style={{flex: "0 100%"}}>
+                    <div style={{flex: "0 100%", marginBottom: '50px'}}>
                         <h2 style={{color: "black", textAlign: 'center'}}>Proposed Solutions</h2>
-                        <h3 style={{color: "black", textAlign: 'center', paddingTop: '30px'}}>Slow-motion for games with action intense sequences</h3>
-                        <p style={{paddingTop: '20px'}}>
-                            Action intensive games or sequences, such as quick time events, can be difficult 
-                            for people with cognitive-related conditions. A way for game developers to help 
-                            with that problem:
-                            <ul className='solution-list'>
-                                <li>Slow down the content</li>
-                                <li>Have an option to skip the content</li>
-                                <li>Don't make the content rely on dexterity</li>
-                            </ul>
-                        </p>
-                        <div>
-                            <div style={{position:'relative', paddingBottom:'calc(56.25% + 44px)'}}>
-                                <iframe src='https://gfycat.com/ifr/GreedyIdenticalBlackrussianterrier' frameborder='0' scrolling='no' width='100%' height='100%' style={{position:'absolute', top: '0', left:'0'}} allowfullscreen></iframe>
-                            </div>
-                            <p>
-                                Regular speed
+                        <div style={{marginBottom: '50px'}}>
+                            <h3 style={{color: "black", textAlign: 'center', paddingTop: '30px'}}>Slow-motion for games with action intense sequences</h3>
+                            <p style={{paddingTop: '20px'}}>
+                                Action intensive games or sequences, such as quick time events, can be difficult 
+                                for people with cognitive-related conditions. A way for game developers to help 
+                                with that problem:
+                                <ol style={{marginTop: '30px'}}>
+                                    <li>Slow down the content</li>
+                                    <li>Have an option to skip the content</li>
+                                    <li>Don't make the content rely on dexterity<sub>1</sub></li>
+                                </ol>
                             </p>
-                            <div style={{position:'relative', paddingBottom:'calc(56.25% + 44px)'}}>
-                                <iframe src='https://gfycat.com/ifr/FineHighlevelHectorsdolphin' frameborder='0' scrolling='no' width='100%' height='100%' style={{position:'absolute', top: '0', left:'0'}} allowfullscreen></iframe>
+                            <div style={{marginBottom: '30px', marginTop: '30px'}}>
+                                <p>
+                                    Regular speed
+                                </p>
+                                <div style={{position:'relative', paddingBottom:'calc(56.25% + 44px)'}}>
+                                    <iframe src='https://gfycat.com/ifr/GreedyIdenticalBlackrussianterrier' frameborder='0' scrolling='no' width='100%' height='100%' style={{position:'absolute', top: '0', left:'0'}} allowfullscreen></iframe>
+                                </div>
+                                <p>
+                                    Slowed down 
+                                </p>
+                                <div style={{position:'relative', paddingBottom:'calc(56.25% + 44px)'}}>
+                                    <iframe src='https://gfycat.com/ifr/FineHighlevelHectorsdolphin' frameborder='0' scrolling='no' width='100%' height='100%' style={{position:'absolute', top: '0', left:'0'}} allowfullscreen></iframe>
+                                </div>
+                                
                             </div>
+                            <h4>Pseudocode Example</h4>
+                            <pre>
+                                <code>
+                                    //To be implemented in the game engine's update function<br></br>
+                                    <br></br>
+                                    timeScale Object = 0.5; <br></br>
+                                    fixedDeltaTime object = 0/02F * timeScale object; <br></br>
+                                    <br></br>
+                                    //To revert time back to normal <br></br>
+                                    <br></br>
+                                    timeScale Object = 1; <br></br>
+                                    fixedDeltaTime Object = 0.02F;
+                                </code>
+                            </pre>
+                            <h4>How To Use</h4>
+                            <ol className="source-list">
+                                <li>
+                                    The above example uses the Unity engine’s objects, but most game 
+                                    engines should have similar time objects.
+                                </li>
+                                <li>
+                                    The code can be called whenever a sequence starts or if the players
+                                    press a button.
+                                </li>
+                            </ol>
                             <p>
-                                Slowed down 
+                                While slowing time down would help players handle the game’s action, it can be difficult 
+                                to implement depending on the game. If it isn’t a regular mechanic in the game, 
+                                then another solution is to remove the action sequence or give players a choice of 
+                                difficulty.
                             </p>
                         </div>
-                        <p>
-                            Code here
-                        </p>
+
+                        <div style={{marginBottom: '50px'}}>
+                            <h3 style={{color: "black", textAlign: 'center', paddingTop: '30px'}}>Difficulty ranges for games</h3>
+                            <p style={{paddingTop: '20px'}}>
+                                While many modern games come with scaling difficulty or difficulty modes, some games do not offer
+                                the choice to players. 
+                            </p>
+                            <h4>How To Implement</h4>
+                            <p>
+                                Any coding solution for this problem would require these changes:
+                                <ol className="source-list">
+                                    <li>Lower requirements for player success (points needed to pass level)</li>
+                                    <li>Lower enemy health and strength if there are enemies</li>
+                                    <li>Give players the choice of difficulty, maybe even the ability to change individual settings<sub>3</sub></li>
+                                </ol>
+                            </p>
+                        </div>
+
+                        <div style={{marginBottom: '50px'}}>
+                            <h3 style={{color: "black", textAlign: 'center', paddingTop: '30px'}}>More obvious interactable items</h3>
+                            <p style={{paddingTop: '20px'}}>
+                                Some game items can be difficult to see, especially if they are placed in a chaotic scene. Some solutions to fix this
+                                would be:
+                                <ol className="source-list">
+                                    <li>Remove nearby items or clutter</li>
+                                    <li>Add more visual effects to the item<sub>1</sub></li>
+                                </ol>
+                            </p>
+                            <h4>How To Implement</h4>
+                            <p>
+                                There are various plugins and particle effects for game engines thatc an be applied to 
+                                game objects.
+                            </p>
+                            <a href="https://www.youtube.com/watch?v=UHCwUMJOFsc">Tutorial for sparkles in Unity</a>
+                        </div>
+                    </div>
+
+                    <div>
+                        <h3 style={{color: 'black'}}>Sources</h3>
+                        <ol className="source-list">
+                            <li>http://gameaccessibilityguidelines.com/</li>
+                            <li>Yuan, Bei, Eelke Folmer, and Frederick C. Harris. "Game accessibility: a survey." Universal Access in the 
+                                Information Society 10.1 (2011): 81-100.</li>
+                            <li>3. Garber, Lee. "Game accessibility: enabling everyone to play." Computer 6 (2013): 14-18.
+                            </li>
+                        </ol>
                     </div>
                 </div>
+
+                
             </div>
 
             <footer class="footer">
