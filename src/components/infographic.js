15a48e7a163d87faa86cd5c8acd37ed1bb69b96b
Alex Wong <alwong34@uw.edu>
2020-05-20 15:19:35 -0700

    fixed infographic spacing and added to motor page

--- src/components/infographic.js
+++ src/components/infographic.js
@@ -6,7 +6,7 @@ const Infographic = () => {
     return (
         <div>
             <div class="skewed-right" style={{background: '#657984', paddingTop: '10px', height: '100px'}}></div>
-            <div style={{position: 'relative', background: '#657984', height: '57rem', width: '100%', zIndex: '0', paddingTop: '5rem'}}>
+            <div style={{position: 'relative', background: '#657984', height: '67rem', width: '100%', zIndex: '0', paddingTop: '5rem', paddingBottom: '5rem'}}>
                 <a href={graphic} target="_blank">
                     <img src={graphic} alt={'accessible gaming infographic'} style={{display: 'block', margin: 'auto', width: '70%'}}></img>
                 </a> 
--- src/pages/motor.js
+++ src/pages/motor.js
@@ -63,15 +63,55 @@ const motor = () => {
                            connected to a 32 bit ARM processor that converts sensor inputs into USB and Bluetooth signals.
                         </p> 
                     </div>
-                    <div style={{flex: "0 100%", marginBottom: '50px'}}>
+                    <div style={{flex: "0 100%", marginBottom: '50px', color: 'black'}}>
                         <h2 style={{color: "black", textAlign: 'center'}}>Proposed Solutions</h2>
-                        <p style={{paddingTop: '20px'}}>
-                            Although default controls can try to specify how designers want their audience to play a game, not all 
-                            users are capable or are comfortable with a  predefined set of controls. One way to help games be more 
-                            accessible is to allow customization of controls and remapping of buttons.
-                        </p>
-                        <div style={{position:'relative', paddingBottom:'calc(56.25% + 44px)'}}>
-                            <iframe src='https://gfycat.com/ifr/PresentRawCrustacean' frameborder='0' scrolling='no' allowfullscreen width='640' height='404'></iframe>
+                        <div>
+                            <h3 style={{color: "black", textAlign: 'center', paddingTop: '30px'}}>Customizable Controls</h3>
+                            <p style={{paddingTop: '10px'}}>
+                                Although default controls can try to specify how designers want their audience to play a game, not all 
+                                users are capable or are comfortable with a  predefined set of controls. One way to help games be more 
+                                accessible is to allow customization of controls and remapping of buttons.
+                            </p>
+                            <div style={{position:'relative', paddingBottom:'1rem'}}>
+                                <iframe src='https://gfycat.com/ifr/PresentRawCrustacean' frameborder='0' scrolling='no' allowfullscreen width='640' height='404'></iframe>
+                            </div>
+                            <h4 style={{textAlign: 'left'}}>How To Implement</h4>
+                            <p style={{textAlign: 'left'}}>
+                                When creating the controls for a game, it is necesssary to create a menu where players can remap and alter the game inputs.
+                                Ideally this would be a section in the settings menu in every game, giving players control options and customizability.
+                            </p>
+                        </div>
+                        <div>
+                        <h3 style={{color: "black", textAlign: 'center', paddingTop: '30px'}}>Variable Aim Assist</h3>
+                            <p style={{paddingTop: '10px'}}>
+                                While aim assist is considered cheating in multiplayer player vs. player games, it can be a strong accessibility tool in
+                                single player or co-op first person shooter games. It helps simplify game combat by locking on to enemies and tracking their
+                                movement. This can lower the reaction time needed to defeat enemies and decrease difficulty considerably.
+                            </p>
+                            <div style={{position:'relative', paddingBottom:'1rem'}}>
+                                <iframe src='https://gfycat.com/ifr/ElectricBlueAsianelephant' frameborder='0' scrolling='no' allowfullscreen width='640' height='404'></iframe>
+                            </div>
+                            <h4 style={{textAlign: 'left'}}>How To Implement</h4>
+                            <h4>Pseudocode Example</h4>
+                            <a href={'https://forum.unity.com/threads/solved-aim-assist-code.49720/'}>Unity Forums</a>
+                            <div>
+                                <iframe src='https://codepen.io/alwong34/embed/oNjJRMB?default-tab=js&amp' style={{height: '300px', width: '70%'}}></iframe>
+                            </div>
+                            <p style={{textAlign: 'left'}}>
+                                One simplified version of an aim assist is to lower mouse sensitivity when the user's crosshairs pass over an enemy. In the code snippet
+                                above, we check to see if the crosshairs collide with an object to shoot, and if it does we reduce the aim sensitivity. This gives the 
+                                feeling that the crosshairs are locked-on to the target.
+                            </p>
+                        </div>
+                        <div>
+                            <h3 style={{color: 'black'}}>Sources</h3>
+                            <ol className="source-list" style={{textAlign: 'left'}}>
+                                <li>https://www.game-accessibility.com/documentation/gaming-with-a-physical-disability/</li>
+                                <li>https://accessible.games/</li>
+                                <li>https://forum.unity.com/threads/solved-aim-assist-code.49720/</li>
+                                <li>https://www.xbox.com/en-US/accessories/controllers/xbox-adaptive-controller</li>
+                                <li>https://www.quadstick.com/shop/quadstick-fps-game-controller</li>
+                            </ol>
                         </div>
                     </div>
                 </div>
