849c6b25a1fdca80ce403970179f0c124b55d53e
Carmelito <carmelitogutierrez@gmail.com>
2020-05-19 20:56:59 -0700

    Updates

---
+++ component---src-pages-index-js-2550c9458cec236f4c6c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{RXBc:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),c=t.n(a),r=t("Bl7J"),l=t("vrFN");t("8ypT");n.default=function(){return c.a.createElement(r.a,null,c.a.createElement(l.a,{title:"Home"}))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-2550c9458cec236f4c6c.js.map
